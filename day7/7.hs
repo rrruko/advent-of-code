694a4c5f10c1a39045c09f772dced9990cc672d7
ruko <rukokarasu@gmail.com>
2017-12-06 23:20:54 -0800

    Name variables better and improve comments

--- day7/7.hs
+++ day7/7.hs
@@ -10,11 +10,11 @@ main = do
 
 part1 :: String -> String
 part1 str =
-    let ts = map (filter isAlpha) . tail . words <$> lines str 
-        -- ^ The right hand side of the arrow on each line
-        hs = head . words <$> lines str
-        -- ^ The left hand side of the arrow on each line
-        isBottom str = all (notElem str) ts 
-        -- ^ A string is the bottom if it never appears on the right hand side of an arrow
-    in  the $ filter isBottom hs
-        -- ^ There should be exactly one word in hs that isBottom
+    let programNames = head . words <$> lines str
+        -- ^ The first word of each line
+        rhs = map (filter isAlpha) . tail . words <$> lines str
+        -- ^ The rest of each line, describing the programs on top
+        isBottom str = all (notElem str) rhs
+        -- ^ A program is the bottom if it isn't on top of anything
+    in  the $ filter isBottom programNames
+        -- ^ There should be exactly one program that isBottom

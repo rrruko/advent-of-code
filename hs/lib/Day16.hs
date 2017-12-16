49928e93865ca972c2dbe3918335f191272cf2ac
ruko <rukokarasu@gmail.com>
2017-12-16 01:16:35 -0800

    Comment day 16

--- hs/lib/Day16.hs
+++ hs/lib/Day16.hs
@@ -31,8 +31,11 @@ type Parser = Parsec () String
 int :: Parser Int
 int = read <$> many digitChar
 
+-- Apply the parsers for each kind of move until all the input is consumed, then
+-- compose all the results.
 parseExpr :: Parser Permutation
-parseExpr = foldr1 (flip (.)) <$> many (parseSpin <|> parseExchange <|> parsePartner)
+parseExpr = foldr1 (flip (.)) <$> 
+    many (parseSpin <|> parseExchange <|> parsePartner)
 
 parseSpin :: Parser Permutation
 parseSpin = char 's' *> (spin <$> int)
@@ -45,22 +48,34 @@ parsePartner = char 'p' *> (partner <$> lowerChar <*> (char '/' *> lowerChar))
 
 main :: IO ()
 main = do
+    -- We don't need the commas or newlines to parse the input
     file <- filter (`notElem` ",\n") <$> readFile "16.txt"
     let ini = V.fromList "abcdefghijklmnop"
     case parseMaybe parseExpr file of
-        Just f  -> do
-            putStrLn $ "Part 1: " <> V.toList (f ini)
-            putStrLn $ "Part 2: " <> V.toList (part2 f ini)
+        Just f -> do
+            putStrLn $ "Part 1: " <> V.fromList (part1 f ini)
+            putStrLn $ "Part 2: " <> V.fromList (part2 f ini)
         Nothing -> putStrLn "Parse failed"
 
+-- For part 1, we just need to apply the permutations to the initial string.
+part1 :: Permutation -> Vector Char -> Vector Char
+part1 f ini = f ini
+
+-- For part 2, we need to apply the permutation one billion times.
+-- Fortunately, a cycle occurs before we hit one billion, so we'll get the same
+-- result by just skipping all the complete cycles.
 part2 :: Permutation -> Vector Char -> Vector Char
 part2 f ini = iterate' f ini (1000000000 `mod` cycleLength [] f ini)
 
+-- Apply a function to a value, remembering every intermediate result. The
+-- length of the cycle is the number of iterations it takes for a result to
+-- appear twice.
 cycleLength :: Eq a => [a] -> (a -> a) -> a -> Int
 cycleLength history f x
     | x `notElem` history = 1 + cycleLength (x:history) f (f x)
     | otherwise           = 0
 
+-- Strict version of iterate
 iterate' :: (a -> a) -> a -> Int -> a
 iterate' f x 0 = x
 iterate' f x n = x `seq` iterate' f (f x) (n - 1)

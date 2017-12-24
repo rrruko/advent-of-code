c740e9e2c8abe245b678c23bfccd68152f15d291
ruko <rukokarasu@gmail.com>
2017-12-23 23:47:08 -0800

    ???

--- hs/lib/Day20.hs
+++ hs/lib/Day20.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns, OverloadedStrings #-}
 
 import Control.Lens
 import Control.Monad
 import Data.Function
 import Data.Functor
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IS
 import Data.List
 import Data.Maybe
 import Data.Void
@@ -57,50 +59,59 @@ a /? b
     | rem a b == 0 = Just (div a b)
     | otherwise    = Nothing
 
-data Roots = Some [Int] | Everywhere
+data Roots = Some IntSet | Everywhere
     deriving (Eq, Show)
 
 (*^*) :: Roots -> Roots -> Roots
-Some xs    *^* Some ys    = Some (xs `intersect` ys)
+Some xs    *^* Some ys    = Some (IS.intersection xs ys)
 Some xs    *^* Everywhere = Some xs
 Everywhere *^* Some ys    = Some ys
 Everywhere *^* Everywhere = Everywhere
 
-empty :: Roots -> Bool
-empty Everywhere = False
-empty (Some xs)  = null xs
+nullR :: Roots -> Bool
+nullR Everywhere = False
+nullR (Some xs)  = IS.null xs
 
--- dioQuadratic a b c tries to solve the equation a²x + bx + c = 0
+fromList = Some . IS.fromList
+empty = Some IS.empty
+
+-- dioQuadratic a b c tries to solve the equation a²x + bx + c = 0 for integer
+-- solutions.
 dioQuadratic :: Int -> Int -> Int -> Roots
 dioQuadratic 0 0 0 = Everywhere
-dioQuadratic 0 0 c = Some []
-dioQuadratic 0 b c = Some $ maybeToList ((-c) /? b)
+dioQuadratic 0 0 c = empty
+dioQuadratic 0 b c = fromList $ maybeToList ((-c) /? b)
 dioQuadratic a b c =
     case isqrt (b*b - 4*a*c) of
-        Just s  -> Some $ catMaybes [((-b) + s) /? (2*a), ((-b) - s) /? (2*a)]
-        Nothing -> Some []
+        Just s  -> fromList $ catMaybes [((-b) + s) /? (2*a), ((-b) - s) /? (2*a)]
+        Nothing -> empty
 
--- The times at which the projections of two 3D particles to an axis collide.
-collideAx :: (Int, Int, Int) -> (Int, Int, Int) -> Roots
-collideAx (pos1, vel1, acc1) (pos2, vel2, acc2) =
-    dioQuadratic (acc2 - acc1) (vel2 - vel1) (pos2 - pos1)
-
+-- The times at which two particles collide on a given axis.
 collideOn :: Particle -> Particle -> Getting Int (V3 Int) Int -> Roots
 collideOn p1 p2 _lens = collideAx (toAxis p1 _lens) (toAxis p2 _lens)
+    where collideAx (pos1, vel1, acc1) (pos2, vel2, acc2) =
+              dioQuadratic (acc2 - acc1) (vel2 - vel1) (pos2 - pos1)
 
 -- Solve the quadratic equation for the difference in particle position on each
 -- axis. Iff they collide on all axes at the same t, then they really collide.
 collides :: Particle -> Particle -> Bool
-collides p q = not $ empty (txs *^* tys *^* tzs)
-    where V3 txs tys tzs = collideOn p q <$> V3 _x _y _z
+collides p q = not . nullR . intersections $ map (collideOn p q) [_x, _y, _z]
+    where intersections = foldr (*^*) Everywhere
 
 -- Subset of particles that eventually collide
 findCollisions :: [Particle] -> [Particle]
-findCollisions xs = [x | x <- xs, collidesAny x xs]
-    where collidesAny x xs = any (\e -> e /= x && collides e x) xs
+findCollisions xs = filter (`collidesAny` xs) xs
 
-part2 :: [Particle] -> Int
-part2 particles = length particles - length (findCollisions particles)
+-- Does x collide any elements of xs besides itself?
+collidesAny :: Particle -> [Particle] -> Bool
+collidesAny x xs = any (\e -> e /= x && collides e x) xs
+
+part2 :: [(Int, Particle)] -> [Int]
+part2 !particles =
+    case allCollisions particles of
+        [] -> part2 (map step particles) 
+        xs -> xs
+    where allCollisions ps = [fst p | p <- ps, q <- ps, position (snd p) == position (snd q), fst p /= fst q]
 
 readParticles :: IO [Particle]
 readParticles = do
@@ -111,4 +122,4 @@ main :: IO ()
 main = do
     particles <- readParticles
     putStrLn $ "Part 1: " ++ show (part1 particles)
-    putStrLn $ "Part 2: " ++ show (part2 particles)
+    putStrLn $ "Part 2: " ++ show (part2 $ zip [0..] particles)

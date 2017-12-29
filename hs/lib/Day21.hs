9e8bd7795860f89c9d498ecd4a7d591c8e94ebe7
rrruko <rukokarasu@gmail.com>
2017-12-28 22:24:20 -0800

    Functions to transform matrices for day 21

--- hs/lib/Day21.hs
+++ hs/lib/Day21.hs
@@ -1,5 +1,6 @@
 module Day21 where
 
+import Control.Applicative
 import Control.Monad
 import Data.List (subsequences, transpose)
 import Data.List.Split
@@ -25,11 +26,23 @@ perms :: Matrix -> [Matrix]
 perms = mapM (appEndo . foldMap Endo) $
     subsequences [reverse, map reverse, transpose]
 
+-- Turn a line from the input file into a function that transforms a matrix
 rule :: String -> Matrix -> Maybe Matrix
 rule str mat
     | from `elem` perms mat = Just to
     | otherwise = Nothing
     where [from, to] = splitOn "/" <$> splitOn " => " str
+
+-- Try applying each of a list of rules to a matrix
+update :: [String] -> Matrix -> Maybe Matrix
+update strs mat = foldr (<|>) Nothing $ map (\str -> rule str mat) strs
+
+divide :: Matrix -> [Matrix]
+divide = concat . fmap (chunksOf 2) . transpose . fmap (chunksOf 2)
+
+-- Combine four quadrants into one matrix
+combine :: Matrix -> Matrix -> Matrix -> Matrix -> Matrix
+combine tl tr bl br = zipWith (++) tl tr ++ zipWith (++) bl br
 
 -- Show that perms works
 main :: IO ()
